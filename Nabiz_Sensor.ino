dc3ce8fc10b80c3b1f9855e0f95e420b9466ce97
AliFurkanDemir <19360859017@ogrenci.btu.edu.tr>
2022-03-08 12:38:35 +0300

    Update Nabiz_Sensor.ino

--- Nabiz_Sensor.ino
+++ Nabiz_Sensor.ino
@@ -1,42 +1,37 @@
-#define USE_ARDUINO_INTERRUPTS true    // Pulse kütüphanesinin daha doğru ölçüm yapabilmesi için bu ayarı etkinleştiriyoruz
-#include <PulseSensorPlayground.h> //Yazının başında bilgisayarımıza kurmuş olduğumuz Pulse Playground kütüphanesini ekliyoruz.
+#define USE_ARDUINO_INTERRUPTS true
+#include <PulseSensorPlayground.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET     4
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
-int nabiz; //İçinde dakikadaki nabzı tutacağımız değişkeni oluşturuyoruz.
+int nabiz;
+const int PulseWire = 0;
+const int LED13 = 13;
+int Threshold = 513;
 
-const int PulseWire = 0; // Pulse sensörümüzü bağlamış olduğumuz Analog pinini belirliyoruz.
-const int LED13 = 13; // Arduino üzerindeki ledin nabzımızla birlikte yanıp sönmesi için bu değişkeni 13 numaralı pin olarak ayarlıyoruz.
-int Threshold = 513; // Belirlemiş olduğumuz eşik değerini bu değişkene atıyoruz.
-
-PulseSensorPlayground pulseSensor; //Sensörümüzü kodumuzda kullanabilmek için onu obje olarak oluşturuyoruz.
+PulseSensorPlayground pulseSensor;
 
 void setup() {
   
-  Serial.begin(9600);  //Bilgisayrımızla olan seri iletişimi başlatıyoruz.
+  Serial.begin(9600);
   
-  pulseSensor.analogInput(PulseWire); //Pulse sensörünün bağlıu olduğu pini belirliyoruz.
-  pulseSensor.blinkOnPulse(LED13);       //arduino üzerindeki ledin nabzımızla yanıp sönmesini istediğimizi söylüyoruz.
-  pulseSensor.setThreshold(Threshold); //Değişkene atamış olduğumuz eşik değerini uyguluyoruz.
-  // put your setup code here, to run once:
+  pulseSensor.analogInput(PulseWire);
+  pulseSensor.blinkOnPulse(LED13);
+  pulseSensor.setThreshold(Threshold);
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for(;;);
   }
 
-  // Draw a single pixel in white
   display.drawPixel(10, 10, WHITE);
 
   display.display();
@@ -44,43 +39,41 @@ void setup() {
 
   display.clearDisplay();
   
-  display.setTextSize(2); // Draw 2X-scale text
+  display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(17, 0);
   display.println(F("Nabziniz"));
-  display.display();      // Show initial text
+  display.display();
   delay(100);
  
   
   if (pulseSensor.begin()) {
     Serial.println("Pulse sensörü objesini yarattık."); 
-  } //Pulse sensörü başarıyla başlatılırsa bilgisayara mesaj gönderioyoruz.
+  }
 
 }
 
 void loop() {
 
-  
-  // put your main code here, to run repeatedly:
-  if (pulseSensor.sawStartOfBeat()) { //Eğer nabız algılarsak
-    nabiz = pulseSensor.getBeatsPerMinute(); //Dakikadaki nabzı nabiz değişkenine kaydediyoruz.
+  if (pulseSensor.sawStartOfBeat()) {
+    nabiz = pulseSensor.getBeatsPerMinute();
 
     display.clearDisplay();
-    display.setTextSize(2); // Draw 2X-scale text
+    display.setTextSize(2);
     display.setTextColor(WHITE);
     display.setCursor(17, 0);
     display.println(F("Nabziniz"));
     display.setCursor(50, 30);
     display.println(nabiz);
-    display.display();      // Show initial text
+    display.display();
     delay(100);
 
     Serial.println("Nabız attı. ");
     Serial.print("BPM: ");                        
-    Serial.println(nabiz); //Dakikdaki nabız değerini aynıo zamanda bilgisayarımıza da gönderiyoruz.
+    Serial.println(nabiz);
   }
 
 
-  delay(20); //20 milisaniye bekletiyoruyuz.
+  delay(20);
 
 }
